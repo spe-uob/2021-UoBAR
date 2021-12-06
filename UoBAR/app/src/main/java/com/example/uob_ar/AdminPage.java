5f2d7e521d6e4feb615c2fde4043a6d2e58325f0
Nolan <_>
2021-12-06 21:29:35 +0000

    enable GPS page

--- UoBAR/app/src/main/java/com/example/uob_ar/AdminPage.java
+++ UoBAR/app/src/main/java/com/example/uob_ar/AdminPage.java
@@ -9,6 +9,6 @@ public class AdminPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_page);
+        setContentView(R.layout.activity_admin);
     }
 }
--- UoBAR/app/src/main/java/com/example/uob_ar/enableGPS.java
+++ UoBAR/app/src/main/java/com/example/uob_ar/enableGPS.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class enableGPS extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting);
+        setContentView(R.layout.activity_enable_gps);
     }
 }
