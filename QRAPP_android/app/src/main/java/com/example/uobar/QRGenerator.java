3d47ef2785c5fd34790f34b82726157aa699aa59
BruHiman <hiamanshubhandari44@gmail.com>
2022-03-11 06:03:39 +0000

    oops forgot to actually ship the UOBAR encoding

--- QRAPP_android/app/src/main/java/com/example/uobar/QRGenerator.java
+++ QRAPP_android/app/src/main/java/com/example/uobar/QRGenerator.java
@@ -6,22 +6,17 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.MediaStore;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 
 import androidmads.library.qrgenearator.QRGContents;
 import androidmads.library.qrgenearator.QRGEncoder;
@@ -46,8 +41,8 @@ public class QRGenerator extends AppCompatActivity {
         saveQRBtn = findViewById(R.id.saveQRBtn);
 
         QRGeneratorBtn.setOnClickListener(v -> {
-            String text = QRInputText.getText().toString();
-            if (text.isEmpty()){
+            String text = "UOBAR" + QRInputText.getText().toString();
+            if (text.replace("UOBAR", "").isEmpty()){
                 Toast.makeText(QRGenerator.this, "Input is required", Toast.LENGTH_SHORT).show();
             } else {
                 QRGEncoder qrgEncoder = new QRGEncoder(text, null, QRGContents.Type.TEXT, 500);
@@ -63,8 +58,8 @@ public class QRGenerator extends AppCompatActivity {
         saveQRBtn.setOnClickListener(v -> {
 
             try {
-                Toast.makeText(getApplicationContext(),"QR code should be in the pictures folder", Toast.LENGTH_SHORT).show();
-                File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "Pictures", QRInputText.getText().toString().replaceAll("[^a-zA-Z0-9]", " ")+".jpg");
+                Toast.makeText(getApplicationContext(),"QR should be saved in your gallery", Toast.LENGTH_SHORT).show();
+                File file = new File(Environment.getExternalStorageDirectory().toString() +File.separator+ "Pictures", QRInputText.getText().toString().replaceAll("[^a-zA-Z0-9]", " ") + ".jpg");
                 FileOutputStream fOut = new FileOutputStream(file);
                 QRMap.compress(Bitmap.CompressFormat.JPEG, 85, fOut);
                 fOut.flush();
