7f965283780e0dbec4ce5e7bf572e05d00fb32a2
Nolan <cn752042807@gmail.com>
2022-03-18 15:38:16 +0000

    add text to audio feature to information page, back & next button

--- QRAPP_android/app/src/main/java/com/example/uobar/GoldneyButton.java
+++ QRAPP_android/app/src/main/java/com/example/uobar/GoldneyButton.java
@@ -2,21 +2,27 @@ package com.example.uobar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class GoldneyButton extends AppCompatActivity {
 
    TextView goldneyInfo;
-   ImageButton  goldneyBttn,next;
+   ImageButton  goldneyBttn,next,back, audio;
+   TextToSpeech t2s;
+
+
    @Override
     protected  void onCreate(Bundle savedInstanceState) {
 
@@ -25,13 +31,40 @@ public class GoldneyButton extends AppCompatActivity {
        goldneyInfo =findViewById(R.id.gardenInfoID);
        goldneyBttn =findViewById(R.id.RoyalID);
 
+       audio = (ImageButton)findViewById(R.id.audio);
+
 
        goldneyInfo.setText("The text which contains information about the Goldney garden");
 
        next = findViewById(R.id.Next_scan);
        next.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(),scan_page.class)));
 
-   }
+
+       t2s = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+               if(status != TextToSpeech.ERROR) {
+                  t2s.setLanguage(Locale.UK);
+               }
+            }
+         });
+         audio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               String toSpeak = goldneyInfo.getText().toString();
+               Toast.makeText(getApplicationContext(), toSpeak,Toast.LENGTH_SHORT).show();
+               t2s.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+            }
+         });
+      }
+
+      public void onPause(){
+         if(t2s !=null){
+            t2s.stop();
+            t2s.shutdown();
+         }
+         super.onPause();
+      }
 
 
 }
--- QRAPP_android/app/src/main/java/com/example/uobar/RoyalFortButton.java
+++ QRAPP_android/app/src/main/java/com/example/uobar/RoyalFortButton.java
@@ -7,12 +7,19 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import android.speech.tts.TextToSpeech;
+import android.widget.Toast;
+
+import java.util.Locale;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RoyalFortButton extends AppCompatActivity {
 
     TextView royalInfo;
-    ImageButton royalBttn, next;
+    ImageButton royalBttn, next, audio, back;
+    TextToSpeech t2s;
+
 
     @Override
     protected  void onCreate(Bundle savedInstanceState) {
@@ -28,5 +35,36 @@ public class RoyalFortButton extends AppCompatActivity {
         next = findViewById(R.id.Next_scan);
         next.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(),scan_page.class)));
 
+        back = findViewById(R.id.info_back);
+        back.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(),MainActivity.class)));
+
+
+        audio = (ImageButton)findViewById(R.id.audio);
+        t2s = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    t2s.setLanguage(Locale.UK);
+                }
+            }
+        });
+        audio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String toSpeak = royalInfo.getText().toString();
+                Toast.makeText(getApplicationContext(), toSpeak,Toast.LENGTH_SHORT).show();
+                t2s.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
     }
+
+    public void onPause(){
+        if(t2s !=null){
+            t2s.stop();
+            t2s.shutdown();
+        }
+        super.onPause();
+    }
+
 }
+
