68d32b1ba4345bcfc58cafe9c04cee6dcaddbcdc
himan <himanshubhandari44@gmail.com>
2022-03-25 13:47:37 +0000

    Shows user location as well as added custom markers

--- QRAPP_android/app/src/main/java/com/example/uobar/MapsActivity.java
+++ QRAPP_android/app/src/main/java/com/example/uobar/MapsActivity.java
@@ -1,7 +1,19 @@
 package com.example.uobar;
 
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -9,23 +21,21 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.example.uobar.databinding.ActivityMapsBinding;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
-    private ActivityMapsBinding binding;
+    Marker usrLoc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityMapsBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
+        setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -45,6 +55,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        }
 
         //temporary variable that has your location will get this value based on gps/user input later
         String curLoc = "RylFort";
@@ -73,12 +87,43 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             );
             mMap.setLatLngBoundsForCameraTarget(rylFortBounds);
 
-            //temp markers for art
-            mMap.addMarker(new MarkerOptions().position(new LatLng(51.45753891274606, -2.6025778327837195)).title("'Follow Me' - Jeppe Hein"));
+            //create image to use on the map
+            Bitmap bMap = addBorder(Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.mirror), 150, 100, false));
+            //temp markers for art using bitmap
+            mMap.addMarker(new MarkerOptions().position(new LatLng(51.45753891274606, -2.6025778327837195)).icon(BitmapDescriptorFactory.fromBitmap(bMap)).title("'Follow Me' - Jeppe Hein")).setTag("https://www.bristol.ac.uk/centenary/look/art/sculpture.html");
+
+
         } else{
             Toast.makeText(getApplicationContext(),"Error fetching location", Toast.LENGTH_LONG).show();
         }
 
 
+
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(@NonNull Marker marker) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse((String)marker.getTag()));
+                startActivity(intent);
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onLocationChanged(Location location){
+        if (usrLoc != null) {
+            usrLoc.remove();
+        }
+        usrLoc = mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())));
+    }
+
+    private Bitmap addBorder(Bitmap bMap) {
+        int padding = 10;
+        Bitmap bMapBorder = Bitmap.createBitmap(bMap.getWidth() + padding * 2, bMap.getHeight() + padding * 2, bMap.getConfig());
+        Canvas canvas = new Canvas(bMapBorder);
+        canvas.drawColor(Color.WHITE);
+        canvas.drawBitmap(bMap, padding, padding, null);
+        return bMapBorder;
     }
 }
--- QRAPP_android/app/src/main/java/com/example/uobar/Welcome.java
+++ QRAPP_android/app/src/main/java/com/example/uobar/Welcome.java
@@ -1,11 +1,13 @@
 package com.example.uobar;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 
 public class Welcome extends AppCompatActivity {
@@ -15,6 +17,7 @@ public class Welcome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         button = (Button)findViewById(R.id.Welcome);
+        ActivityCompat.requestPermissions(Welcome.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
