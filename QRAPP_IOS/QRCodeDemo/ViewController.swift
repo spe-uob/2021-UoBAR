4903d92fc77f299ea6de3da03b06bff6805066a8
DiAbyss <61198709+DiAbyss@users.noreply.github.com>
2022-04-22 19:46:03 +0800

    ios demo

---
+++ QRAPP_IOS/QRCodeDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRCodeDemo
+//
+//  Created by Eleven on 4/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        title = "QR Code"
+    }
+
+
+}
+
